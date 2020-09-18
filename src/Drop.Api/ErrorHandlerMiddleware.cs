e5a87d95ccfd09b77e048c547ca5c82a55644668
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-09-18 14:07:53 +0200

    exceptions

--- src/Drop.Api/ErrorHandlerMiddleware.cs
+++ src/Drop.Api/ErrorHandlerMiddleware.cs
@@ -1,13 +1,23 @@
 using System;
 using System.Threading.Tasks;
+using Drop.Application.Exceptions;
+using Drop.Core.Exceptions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Drop.Api
 {
     public class ErrorHandlerMiddleware : IMiddleware
     {
         private readonly ILogger<ErrorHandlerMiddleware> _logger;
+
+        private static readonly JsonSerializerSettings JsonSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            Formatting = Formatting.Indented
+        };
 
         public ErrorHandlerMiddleware(ILogger<ErrorHandlerMiddleware> logger)
         {
@@ -23,8 +33,28 @@ namespace Drop.Api
             catch (Exception exception)
             {
                 _logger.LogError(exception, exception.Message);
-                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                switch (exception)
+                {
+                    case AppException appException:
+                        await HandleCustomException(context, appException.Code, appException.Message);
+                        return;
+                    case DomainException domainException:
+                        await HandleCustomException(context, domainException.Code, domainException.Message);
+                        return;
+                    default:
+                        throw;
+                }
             }
+        }
+
+        private static async Task HandleCustomException(HttpContext context, string code, string message)
+        {
+            context.Response.StatusCode = 400;
+            var response = new
+            {
+                code, message
+            };
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(response, JsonSettings));
         }
     }
 }
